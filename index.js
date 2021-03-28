1d623020d8796989f24c12d49dfdd45d3de375d3
Sheldon Stevens <sgste080@yahoo.com>
2021-03-28 12:49:59 -0700

    making progress

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-// Prompt the user for their name, preferred method of communication, and known languages
-// YOUR CODE HERE
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        message: "What is your GitHub username?",
+        name: 'username',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a valid GitHub username.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your GitHub repo?",
+        name: 'repo',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter a valid repository name.");
+            }
+            return true;
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Completed!")
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
